a51f01156f42671f031812f8f306081a016e7940
Serhii Lukianenko <lukyanenko88@gmail.com>
2021-09-15 20:13:40 +0300

    now you can create new users with username nad password and use those fields for login to application

--- src/main/java/com/deo/din/controller/RegistrationController.java
+++ src/main/java/com/deo/din/controller/RegistrationController.java
@@ -20,20 +20,20 @@ public class RegistrationController {
 
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Model model){
+    public String addUser(User user, Model model) {
 
         User userFromDb = userRepository.findByUsername(user.getUsername());
-        if(userFromDb==null){
+        if (userFromDb == null) {
             user.setActive(true);
             user.setRoles(Collections.singleton(Role.USER));
             userRepository.save(user);
-        }else {
-            model.addAttribute("message","User exists");
+        } else {
+            model.addAttribute("message", "User exists");
             return "/registration";
         }
 
